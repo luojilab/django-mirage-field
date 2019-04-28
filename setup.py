5d968364150007106f9955726e94f5a17aaaa2d6
tcitry <tcitry@gmail.com>
2019-04-28 12:58:27 +0800

    update version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='django-mirage-field',
-    version='0.0.1',
+    version='0.0.2',
     install_requires=[
         "cryptography",
         "tqdm",
