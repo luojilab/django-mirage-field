1d613b29ba5cb3e799ddbf57ee668a65e7d8e267
tcitry <tcitry@gmail.com>
2019-04-28 12:26:02 +0800

    enhancement key length check

--- mirage/crypto.py
+++ mirage/crypto.py
@@ -13,8 +13,11 @@ class Crypto:
     def __init__(self, key=None):
         if key is None:
             key = getattr(settings, "SECRET_KEY")
-        assert len(key) >= 32, "key length must larger than 32!"
-        self.key = base64.urlsafe_b64encode(force_bytes(key))[:32]
+            assert len(key) >= 32, "settings.SECRET_KEY length must more than 32!"
+            self.key = base64.urlsafe_b64encode(force_bytes(key))[:32]
+        else:
+            assert len(key) >= 32, "key length must more than 32!"
+            self.key = key[:32]
 
     def encrypt(self, text):
         if text is None:
