330bc14b0ae831f75fe5739c5af42ecd1f34321f
yindongliang <yindongliang@luojilab.com>
2019-09-18 17:35:00 +0800

    update to 0.0.3 version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='django-mirage-field',
-    version='0.0.2',
+    version='0.0.3',
     install_requires=[
         "cryptography",
         "tqdm",
