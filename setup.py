0ab33d434a74b152de269806717f85fe649a8369
tcitry <tcitry@gmail.com>
2020-01-09 19:38:49 +0800

    fix django3.0 from_db_value context param

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='django-mirage-field',
-    version='1.0.0',
+    version='1.0.1',
     install_requires=[
         "cryptography",
         "tqdm",
