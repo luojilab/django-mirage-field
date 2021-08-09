f5b69c140a56e86cb129705f0c9c6ccbc25ee59c
Emiliano Beronich <emilianoberonich@gmail.com>
2021-08-09 10:10:36 -0300

    Add customized primary key field to Command

--- mirage/management/commands/mirage.py
+++ mirage/management/commands/mirage.py
@@ -15,29 +15,41 @@ class Command(BaseCommand):
         parser.add_argument("--total", type=int, required=False)
         parser.add_argument("--limit", type=int, required=False)
         parser.add_argument("--tofield", type=str, required=False)
+        parser.add_argument("--idfield", type=str, required=False)
 
     def handle(self, *args, **options):
-        app = options['app']
-        model = options['model']
-        field = options['field']
-        method = options['method']
-        offset = options['offset'] or 0
-        total = options['total']
-        limit = options['limit'] or 1000
-        tofield = options['tofield']
+        app = options["app"]
+        model = options["model"]
+        field = options["field"]
+        method = options["method"]
+        offset = options["offset"] or 0
+        total = options["total"]
+        limit = options["limit"] or 1000
+        tofield = options["tofield"]
+        idfield = options["idfield"] or "id"
 
-        if method == 'encrypt':
-            Migrator(app, model, field).encrypt(offset=offset, total=total, limit=limit)
-        elif method == 'decrypt':
-            Migrator(app, model, field).decrypt(offset=offset, total=total, limit=limit)
-        elif method == 'copy_to':
+        if method == "encrypt":
+            Migrator(app, model, field, idfield=idfield).encrypt(
+                offset=offset, total=total, limit=limit
+            )
+        elif method == "decrypt":
+            Migrator(app, model, field, idfield=idfield).decrypt(
+                offset=offset, total=total, limit=limit
+            )
+        elif method == "copy_to":
             assert tofield is not None
-            Migrator(app, model, field, tofield=tofield).copy_to(offset=offset, total=total, limit=limit)
-        elif method == 'decrypt_to':
+            Migrator(app, model, field, tofield=tofield, idfield=idfield).copy_to(
+                offset=offset, total=total, limit=limit
+            )
+        elif method == "decrypt_to":
             assert tofield is not None
-            Migrator(app, model, field, tofield=tofield).decrypt_to(offset=offset, total=total, limit=limit)
-        elif method == 'encrypt_to':
+            Migrator(app, model, field, tofield=tofield, idfield=idfield).decrypt_to(
+                offset=offset, total=total, limit=limit
+            )
+        elif method == "encrypt_to":
             assert tofield is not None
-            Migrator(app, model, field, tofield=tofield).encrypt_to(offset=offset, total=total, limit=limit)
+            Migrator(app, model, field, tofield=tofield, idfield=idfield).encrypt_to(
+                offset=offset, total=total, limit=limit
+            )
         else:
             return
